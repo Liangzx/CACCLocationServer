65c0fd27c0202a056f71b6c28c482bcba9aa27ab
Alias <able_lzx@live.com>
2017-01-05 23:21:56 +0800

    1.xx

--- CACCLocationServer/cacc_location_session_nmea.h
+++ CACCLocationServer/cacc_location_session_nmea.h
@@ -14,7 +14,7 @@ public:
 	~CACCLocationSessionNMEA();
 	virtual void do_write(std::size_t length);
 private:
-	std::string std::string do_format(std::string && str);
+	std::string do_format(std::string && str);
 };
 #endif // !_CACC_LOCATION_SESSION_NMEA_H_
 
